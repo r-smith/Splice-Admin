51f5dc7c8977e40f76896c87e0b0e7b6b4a99c1a
Ryan Smith <ryan@smithlv.com>
2016-12-31 12:56:15 -0800

    Applications are now retrieved through WMI StdRegProv with a fallback to remote registry.

--- Splice Admin/Classes/RemoteApplication.cs
+++ Splice Admin/Classes/RemoteApplication.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Management;
 using System.ServiceProcess;
+using System.Windows;
 
 namespace Splice_Admin.Classes
 {
@@ -24,41 +27,186 @@ namespace Splice_Admin.Classes
             var taskResult = new TaskResult();
             Result = taskResult;
 
-            const string uninstallKey64 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
-            const string uninstallKey32 = @"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
-            const string serviceName = "RemoteRegistry";
-            bool isLocal = ComputerName.ToUpper() == Environment.MachineName.ToUpper() ? true : false;
-            bool isServiceRunning = true;
+            const string uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            const string uninstallKey32on64 = @"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
 
-            // If the target computer is remote, then start the Remote Registry service.
-            using (
-                GlobalVar.UseAlternateCredentials
-                ? UserImpersonation.Impersonate(GlobalVar.AlternateUsername, GlobalVar.AlternateDomain, GlobalVar.AlternatePassword)
-                : null)
-            using (var sc = new ServiceController(serviceName, ComputerName))
+            var managementScope = new ManagementScope($@"\\{ComputerName}\root\CIMV2");
+            ManagementBaseObject inParams = null;
+            ManagementBaseObject outParams = null;
+
+            try
+            {
+                using (var wmiRegistry = new ManagementClass(managementScope, new ManagementPath("StdRegProv"), null))
+                {
+                    List<string> subKeys = null;
+                    List<string> subKeys32on64 = null;
+                    var uninstallKeys = new List<string>();
+
+                    // Get uninstall subkeys.
+                    inParams = wmiRegistry.GetMethodParameters("EnumKey");
+                    inParams["sSubKeyName"] = uninstallKey;
+                    outParams = wmiRegistry.InvokeMethod("EnumKey", inParams, null);
+                    if (outParams["sNames"] != null)
+                        subKeys = new List<string>((string[])outParams["sNames"]).Select(x => $@"{uninstallKey}\{x}").ToList();
+
+                    // Get 32-bit on 64-bit uninstall subkeys.
+                    inParams["sSubKeyName"] = uninstallKey32on64;
+                    outParams = wmiRegistry.InvokeMethod("EnumKey", inParams, null);
+                    if (outParams["sNames"] != null)
+                        subKeys32on64 = new List<string>((string[])outParams["sNames"]).Select(x => $@"{uninstallKey32on64}\{x}").ToList();
+
+                    // Combine lists of keys.
+                    if (subKeys != null)
+                        uninstallKeys.AddRange(subKeys);
+                    if (subKeys32on64 != null)
+                        uninstallKeys.AddRange(subKeys32on64);
+
+                    // Enumerate keys.
+                    foreach (string subKey in uninstallKeys)
+                    {
+                        // Get SystemComponent (DWORD) value.  Skip key if this value exists and is set to '1'.
+                        inParams = wmiRegistry.GetMethodParameters("GetDWORDValue");
+                        inParams["sSubKeyName"] = subKey;
+                        inParams["sValueName"] = "SystemComponent";
+                        outParams = wmiRegistry.InvokeMethod("GetDWORDValue", inParams, null);
+                        if (outParams["uValue"] != null && (UInt32)outParams["uValue"] == 1)
+                            continue;
+
+                        // Get ParentKeyName (String) value.  Skip key if this value exists.
+                        inParams = wmiRegistry.GetMethodParameters("GetStringValue");
+                        inParams["sSubKeyName"] = subKey;
+                        inParams["sValueName"] = "ParentKeyName";
+                        outParams = wmiRegistry.InvokeMethod("GetStringValue", inParams, null);
+                        if (outParams["sValue"] != null && ((string)outParams["sValue"]).Length > 0)
+                            continue;
+
+                        // Get ReleaseType (String) value.  Skip key if this value contains 'Update' or 'Hotfix'.
+                        inParams["sSubKeyName"] = subKey;
+                        inParams["sValueName"] = "ReleaseType";
+                        outParams = wmiRegistry.InvokeMethod("GetStringValue", inParams, null);
+                        if (outParams["sValue"] != null && (((string)outParams["sValue"]).Contains("Update") || ((string)outParams["sValue"]).Equals("Hotfix")))
+                            continue;
+
+                        var app = new RemoteApplication();
+
+                        // Get DisplayName (String) value.
+                        inParams["sSubKeyName"] = subKey;
+                        inParams["sValueName"] = "DisplayName";
+                        outParams = wmiRegistry.InvokeMethod("GetStringValue", inParams, null);
+                        if (outParams["sValue"] != null)
+                            app.DisplayName = (string)outParams["sValue"];
+                        else
+                            continue;
+
+                        // Get Publisher (String) value.
+                        inParams["sSubKeyName"] = subKey;
+                        inParams["sValueName"] = "Publisher";
+                        outParams = wmiRegistry.InvokeMethod("GetStringValue", inParams, null);
+                        if (outParams["sValue"] != null)
+                            app.Publisher = (string)outParams["sValue"];
+
+                        // Get DisplayVersion (String) value.
+                        inParams["sSubKeyName"] = subKey;
+                        inParams["sValueName"] = "DisplayVersion";
+                        outParams = wmiRegistry.InvokeMethod("GetStringValue", inParams, null);
+                        if (outParams["sValue"] != null)
+                            app.Version = (string)outParams["sValue"];
+
+                        // Get UninstallString (String) value.
+                        inParams["sSubKeyName"] = subKey;
+                        inParams["sValueName"] = "UninstallString";
+                        outParams = wmiRegistry.InvokeMethod("GetStringValue", inParams, null);
+                        if (outParams["sValue"] != null)
+                            app.UninstallPath = (string)outParams["sValue"];
+
+                        apps.Add(app);
+                    }
+                }
+
+                taskResult.DidTaskSucceed = true;
+            }
+
+            catch (ManagementException ex) when (ex.ErrorCode == ManagementStatus.NotFound)
+            {
+                // Target OS might not support WMI StdRegProv.  Attempt to gather data using remote registry.
+                apps = new List<RemoteApplication>();
+                const string serviceName = "RemoteRegistry";
+                bool isLocal = ComputerName.ToUpper() == Environment.MachineName.ToUpper() ? true : false;
+                bool isServiceRunning = true;
+
+                // If the target computer is remote, then start the Remote Registry service.
+                using (
+                    GlobalVar.UseAlternateCredentials
+                    ? UserImpersonation.Impersonate(GlobalVar.AlternateUsername, GlobalVar.AlternateDomain, GlobalVar.AlternatePassword)
+                    : null)
+                using (var sc = new ServiceController(serviceName, ComputerName))
+                {
+                    try
+                    {
+                        if (!isLocal && sc.Status != ServiceControllerStatus.Running)
+                        {
+                            isServiceRunning = false;
+                            sc.Start();
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+
+                    try
+                    {
+                        using (RegistryKey key = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, ComputerName))
+                        {
+                            using (RegistryKey mainKey64 = key.OpenSubKey(uninstallKey))
+                                apps.AddRange(EnumerateUninstallKeys(mainKey64));
+                            using (RegistryKey mainKey32 = key.OpenSubKey(uninstallKey32on64))
+                                apps.AddRange(EnumerateUninstallKeys(mainKey32));
+                        }
+
+                        taskResult.DidTaskSucceed = true;
+                    }
+                    catch
+                    {
+                        taskResult.DidTaskSucceed = false;
+                    }
+
+
+                    // Cleanup.
+                    if (!isLocal && !isServiceRunning)
+                    {
+                        try
+                        {
+                            if (sc != null)
+                            {
+                                sc.Stop();
+                            }
+                        }
+
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+            }
+
+            catch
+            {
+                // Do nothing.
+            }
+
+            finally
+            {
+                if (inParams != null)
+                    inParams.Dispose();
+                if (outParams != null)
+                    outParams.Dispose();
+            }
+
+            // Get Internet Explorer version.
+            if (taskResult.DidTaskSucceed && apps.Count > 0)
             {
                 try
                 {
-                    if (!isLocal && sc.Status != ServiceControllerStatus.Running)
-                    {
-                        isServiceRunning = false;
-                        sc.Start();
-                    }
-                }
-                catch (Exception)
-                {
-                }
-
-                try
-                {
-                    using (RegistryKey key = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, ComputerName))
-                    {
-                        using (RegistryKey mainKey64 = key.OpenSubKey(uninstallKey64))
-                            apps.AddRange(EnumerateUninstallKeys(mainKey64));
-                        using (RegistryKey mainKey32 = key.OpenSubKey(uninstallKey32))
-                            apps.AddRange(EnumerateUninstallKeys(mainKey32));
-                    }
-
                     var internetExplorerVersion = FileVersionInfo.GetVersionInfo($@"\\{ComputerName}\C$\Program Files\Internet Explorer\iexplore.exe");
                     if (internetExplorerVersion != null && internetExplorerVersion.ProductVersion.Length > 0)
                     {
@@ -69,30 +217,8 @@ namespace Splice_Admin.Classes
                             Version = internetExplorerVersion.ProductVersion
                         });
                     }
-
-                    taskResult.DidTaskSucceed = true;
                 }
-                catch
-                {
-                    taskResult.DidTaskSucceed = false;
-                }
-
-
-                // Cleanup.
-                if (!isLocal && !isServiceRunning)
-                {
-                    try
-                    {
-                        if (sc != null)
-                        {
-                            sc.Stop();
-                        }
-                    }
-
-                    catch (Exception)
-                    {
-                    }
-                }
+                catch { }
             }
 
             return apps;
